631969ae497b821a8c5a8b6d2a832bbfbdffb6da
Nelson Boyer <nboyer@google.com>
2021-09-02 20:43:20 -0700

    Made it so each test case is displayed in JUnit results. Also fixed a bug causing failed runs to appear as passed.

--- src/main/java/com/pdsl/junit/PdslGherkinJUnit4Runner.java
+++ src/main/java/com/pdsl/junit/PdslGherkinJUnit4Runner.java
@@ -6,7 +6,6 @@ import com.pdsl.executors.TraceableTestRunExecutor;
 import com.pdsl.gherkin.DefaultGherkinTestSpecificationFactory;
 import com.pdsl.gherkin.specifications.GherkinTestSpecificationFactory;
 import com.pdsl.reports.MetadataTestRunResults;
-import com.pdsl.reports.asciidoctor.AsciidoctorReportGenerator;
 import com.pdsl.specifications.FileSystemTestResourceFinder;
 import com.pdsl.specifications.GlobPathMatcher;
 import com.pdsl.specifications.TestResourceFinder;
@@ -17,44 +16,29 @@ import com.pdsl.testcases.TestCaseFactory;
 import com.pdsl.transformers.DefaultPolymorphicDslPhraseFilter;
 import com.pdsl.transformers.PolymorphicDslPhraseFilter;
 import org.antlr.v4.runtime.tree.ParseTreeListener;
-import org.asciidoctor.AttributesBuilder;
-import org.asciidoctor.OptionsBuilder;
-import org.asciidoctor.Placement;
-import org.asciidoctor.SafeMode;
-import org.junit.AssumptionViolatedException;
 import org.junit.Test;
-import org.junit.internal.runners.model.EachTestNotifier;
 import org.junit.runner.Description;
 import org.junit.runner.notification.RunNotifier;
-import org.junit.runner.notification.StoppedByUserException;
 import org.junit.runners.BlockJUnit4ClassRunner;
-import org.junit.runners.ParentRunner;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.InitializationError;
-import org.junit.runners.model.Statement;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.file.Path;
 import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class PdslGherkinJUnit4Runner extends BlockJUnit4ClassRunner {
-
-    private static final TestCaseFactory testCaseFactory = new PreorderTestCaseFactory();
     private final Map<String, Collection<MetadataTestRunResults>> metaDataTestRunResults = new HashMap<>();
     private final String context;
     private final String applicationName;
     private final String resourceRoot;
     private final Collection<MetadataTestRunResults> results = new ArrayList<>();
-
+    private static final TestCaseFactory testCaseFactory = new PreorderTestCaseFactory();
 
     public Map<String, Collection<MetadataTestRunResults>> getMetaDataTestRunResults() {
         return Map.of(context, results);
@@ -62,7 +46,7 @@ public class PdslGherkinJUnit4Runner extends BlockJUnit4ClassRunner {
 
     /**
      * Returns the methods that run tests.
-     *
+     * <p>
      * This includes all standard JUnit4 methods annotated with @Test as well as any annotated with @PdslTest.
      */
     @Override
@@ -72,34 +56,6 @@ public class PdslGherkinJUnit4Runner extends BlockJUnit4ClassRunner {
         frameworkMethods.addAll(
                 getTestClass().getAnnotatedMethods(PdslTest.class));
         return List.copyOf(frameworkMethods);
-    }
-
-    @Override
-    protected void runChild(final FrameworkMethod method, RunNotifier notifier) {
-        PdslTest pdslTest = method.getAnnotation(PdslTest.class);
-        if (pdslTest != null) {
-            Description description = describeChild(method);
-            if (isIgnored(method)) {
-                notifier.fireTestIgnored(description);
-            } else {
-                final ParseTreeListener parseTreeListener = getParseTreeListener(pdslTest);
-                final Collection<TestCase> testCases = getTestCases(pdslTest);
-                //TODO: Allow the input streams for the executor to be customized
-                final TraceableTestRunExecutor executor = new DefaultPolymorphicDslTestExecutor();
-                Preconditions.checkArgument(!testCases.isEmpty(),
-                        "Somehow no test cases were produced from the features! This is likely an error with the PDSL framework");
-                PdslStatement statement = new PdslStatement(testCases, parseTreeListener, context, executor);
-                runLeaf(statement, description, notifier);
-                Optional<MetadataTestRunResults> runResults = statement.getResults();
-                if (runResults.isPresent()) {
-                    results.add(statement.getResults().orElseThrow());
-                } else {
-                    throw new IllegalStateException("The PDSL Test Run did not produce any results!");
-                }
-            }
-        } else {
-            super.runChild(method, notifier);
-        }
     }
 
     private Collection<TestCase> getTestCases(PdslTest pdslTest) {
@@ -121,6 +77,72 @@ public class PdslGherkinJUnit4Runner extends BlockJUnit4ClassRunner {
         return testCaseFactory.processTestSpecification(gherkinTestSpecifications.get());
     }
 
+    private Set<URL> getTestResources(PdslTest pdslTest) {
+        Set<URL> testResources = new HashSet<>();
+        // Find the files we will be testing
+        PathMatcher pathMatcher = new GlobPathMatcher(getGlobResourcePaths(pdslTest.includesResources()),
+                getGlobResourcePaths(pdslTest.excludesResources()));
+        TestResourceFinder finder = new FileSystemTestResourceFinder(pathMatcher);
+        try {
+
+            Optional<Collection<URL>> resources = finder.scanForTestResources(Paths.get(resourceRoot).toUri().toURL());
+            if (resources.isPresent()) {
+                testResources.addAll(resources.get());
+            }
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException(String.format(
+                    "Could not find test resources when searching file system at:%n\t%s", resourceRoot),
+                    e);
+        }
+        if (testResources.isEmpty()) {
+            throw new IllegalArgumentException(String.format(
+                    "No feature files were found!%n\tResource Root: %s%n%n\tincluding: %s%n%n\texcluding: %s",
+                    resourceRoot, Arrays.deepToString(pdslTest.includesResources()), Arrays.deepToString(pdslTest.excludesResources())));
+        }
+        return testResources;
+    }
+
+    private List<String> getGlobResourcePaths(String[] resources) {
+        String root = !resourceRoot.endsWith("/") ? "**/" + resourceRoot + "/" : "**/" + resourceRoot;
+        return Arrays.stream(resources).map(root::concat).collect(Collectors.toList());
+    }
+
+    @Override
+    public void run(RunNotifier runNotifier) {
+        for (FrameworkMethod method : getChildren()) {
+            if (isIgnored(method)) {
+                runNotifier.fireTestIgnored(describeChild(method));
+            } else {
+                runChild(method, runNotifier);
+            }
+        }
+    }
+
+    @Override
+    protected void runChild(final FrameworkMethod method, RunNotifier notifier) {
+        PdslTest pdslTest = method.getAnnotation(PdslTest.class);
+        if (pdslTest != null) {
+            ParseTreeListener parseTreeListener = getParseTreeListener(pdslTest);
+            final Collection<TestCase> testCases = getTestCases(pdslTest);
+            //TODO: Allow the input streams for the executor to be customized
+            final TraceableTestRunExecutor executor = new DefaultPolymorphicDslTestExecutor();
+            try {
+            Preconditions.checkArgument(!testCases.isEmpty(),
+                    "Somehow no test cases were produced from the features! This is likely an error with the PDSL framework");
+
+                PdslExecutorRunner pdslExecutorRunner = new PdslExecutorRunner(getTestClass().getJavaClass(),
+                        parseTreeListener, getTestCases(pdslTest), executor);
+                pdslExecutorRunner.run(notifier);
+                results.addAll(pdslExecutorRunner.getMetadataTestRunResults());
+            } catch (InitializationError initializationError) {
+                throw new IllegalStateException("Could not intialize PdslExecutorRunner!", initializationError);
+            }
+        } else {
+            super.runChild(method, notifier);
+        }
+    }
+
+
     private ParseTreeListener getParseTreeListener(PdslTest pdslTest) {
         try {
             //TODO: Allow a provider to pass the parsetreelistener for more complicated implementations
@@ -135,39 +157,10 @@ public class PdslGherkinJUnit4Runner extends BlockJUnit4ClassRunner {
         }
     }
 
-    private List<String> getGlobResourcePaths(String[] resources) {
-        String root = !resourceRoot.endsWith("/") ? "**/" + resourceRoot + "/" : "**/" + resourceRoot;
-        return Arrays.stream(resources).map(root::concat).collect(Collectors.toList());
-    }
-    private Set<URL> getTestResources(PdslTest pdslTest) {
-        Set<URL> testResources = new HashSet<>();
-        // Find the files we will be testing
-        PathMatcher pathMatcher = new GlobPathMatcher(getGlobResourcePaths(pdslTest.includesResources()),
-                getGlobResourcePaths(pdslTest.excludesResources()));
-        TestResourceFinder finder = new FileSystemTestResourceFinder(pathMatcher);
-        try {
-
-                Optional<Collection<URL>> resources = finder.scanForTestResources(Paths.get(resourceRoot).toUri().toURL());
-                if (resources.isPresent()) {
-                    testResources.addAll(resources.get());
-                }
-        }
-        catch (MalformedURLException e) {
-            throw new IllegalArgumentException(String.format(
-                    "Could not find test resources when searching file system at:%n\t%s", resourceRoot),
-                    e);
-        }
-        if (testResources.isEmpty()) {
-            throw new IllegalArgumentException(String.format(
-                    "No feature files were found!%n\tResource Root: %s%n%n\tincluding: %s%n%n\texcluding: %s",
-                    resourceRoot, Arrays.deepToString(pdslTest.includesResources()), Arrays.deepToString(pdslTest.excludesResources())));
-        }
-        return testResources;
-    }
-
     public String getApplicationName() {
         return applicationName;
     }
+
     public PdslGherkinJUnit4Runner(Class<?> testClass) throws InitializationError {
         super(testClass);
         PdslGherkinApplication annotation = testClass.getAnnotation(PdslGherkinApplication.class);
