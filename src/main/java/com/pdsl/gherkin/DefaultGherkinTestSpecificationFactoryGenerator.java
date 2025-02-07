74b052af9d2de5a25c3326f7494d5016ea6cfbaf
Nelson <62027792+incident-recipient@users.noreply.github.com>
2025-02-07 12:56:00 -0700

    Update DefaultGherkinTestSpecificationFactoryGenerator.java

--- src/main/java/com/pdsl/gherkin/DefaultGherkinTestSpecificationFactoryGenerator.java
+++ src/main/java/com/pdsl/gherkin/DefaultGherkinTestSpecificationFactoryGenerator.java
@@ -4,6 +4,16 @@ import com.pdsl.runners.TestSpecificationFactoryGenerator;
 import com.pdsl.specifications.TestSpecificationFactory;
 import com.pdsl.transformers.PolymorphicDslPhraseFilter;
 
+/** A factory for GherkinTestSpecificationFactories.
+*
+* Gherkin creates "Pickles" which are eventually turned into test cases. PDSL uses a PickleJarFactory and other constructs
+* to process feature files. However, it is not possible for PDSL to parse these feature files unless it has a
+* PolymorphicDslPhraseFilter which is not available at compile time.
+*
+* The DefaultGherkinTestSpecificaitonFactory works well, but if code actions need to be taken while it is being
+* processed (or if it simply needs a separate implementation for some of its components) then this generator
+* is necessary to allow that modification.
+*/
 public class DefaultGherkinTestSpecificationFactoryGenerator implements
     TestSpecificationFactoryGenerator {
 
