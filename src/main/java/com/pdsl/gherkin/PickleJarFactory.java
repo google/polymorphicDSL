67946bc7ed5b164d5d9ad6700d2f200198f88663
Jana S <janaarthanan@google.com>
2025-02-18 12:32:09 +0000

    removed subs from pdsl

--- src/main/java/com/pdsl/gherkin/PickleJarFactory.java
+++ src/main/java/com/pdsl/gherkin/PickleJarFactory.java
@@ -296,27 +296,8 @@ public class PickleJarFactory implements GherkinObservable {
     @Override
     public void notifyObservers(String title, List<String> steps, Set<String> tags,
         Map<String, String> substitutions) {
-        addTag(tags, substitutions, "xray-test-plan");
-        addTag(tags, substitutions, "xray-test-case");
-        addTag(tags, substitutions, "xray-test-platform");
-        addTag(tags, substitutions, "xray-test-env");
         for (GherkinObserver observer : observers) {
             observer.onScenarioConverted(title, steps, tags, substitutions);
-        }
-    }
-
-    /**
-     * Adds a tag to the given set of tags if a value for the specified key exists in the
-     * substitutions map.
-     *
-     * @param tags The set of tags to which the new tag will be added.
-     * @param substitutions A map containing key-value pairs for substitutions.
-     * @param key The key for the tag to be added.
-     */
-    private void addTag(Set<String> tags, Map<String, String> substitutions, String key) {
-        String value = substitutions.get(key);
-        if (value != null) {
-            tags.add("@%s=%s".formatted(key, value));
         }
     }
 }
